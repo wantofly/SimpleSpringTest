864ccee745525123b120174de39e5b2a9f2d73e4
wantofly <conixfans@gmail.com>
2014-09-30 07:52:09 +0800

    nothing

--- src/main/java/com/cginfo/test/simplespring/Bean1.java
+++ src/main/java/com/cginfo/test/simplespring/Bean1.java
@@ -2,18 +2,18 @@ package com.cginfo.test.simplespring;
 
 import org.springframework.beans.factory.annotation.Value;
 
-public class Bean1 implements IBean1 {
+public class Bean1 implements IBean1{
+	
+	private String var1;
+	
+	@Override
+	public String hello(String val){
+		return var1+", "+val;
+	}
 
-    private String var1;
-
-    @Override
-    public String hello(String name) {
-        return var1 + ", " + name;
-    }
-
-    @Override
+	@Override
     @Value("${var1}")
-    public void setVar1(String var1) {
-        this.var1 = var1;
-    }
+	public void setVar1(String arg1) {
+		this.var1 = arg1;
+	}
 }
